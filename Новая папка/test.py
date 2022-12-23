76d4953d84e267afe5a4c8d32c194345b0d939b2
liliakanukova <115219418+liliakanukova@users.noreply.github.com>
2022-12-23 20:57:04 +0300

    Update test.py

--- Новая папка/test.py
+++ Новая папка/test.py
@@ -1,9 +1,14 @@
-from project import *
+from project import decrypt_vigner,encrypt_cezar,decrypt_cezar
   
-def encrypt_vygner():
-    assert encrypt_vygner('забег','пришел' == 'шрйыжф')
-def decrypt_vigner():
+def test_decrypt_vigner():
     assert encrypt_vygner('забег','шрйыжф' == 'пришел')
-
-def encrypt_cezar(message,key):
-    assert encrypt_vygner('место','прише' == 'шрйыжф')
+def test_encrypt_vigner():
+    assert encrypt_vygner('забег','пришел' == 'шрйыжф')
+def test_encode_atbash():
+    assert  encode_atbash('aceg' == 'zwtr')
+def test_encode_atbash():
+    assert  encode_atbash('zwtr' == 'aceg')   
+def test_encrypt_cezar(message,key):
+    assert encrypt_vygner('место', 3 == 'пзфхс')
+def test_decrypt_cezar(message,key):
+    assert encrypt_vygner('пзфхс', 3 == 'место')
