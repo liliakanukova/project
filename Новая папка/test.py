67a7859f7c7bdbc8df7471b2bc9a872d2ca827f3
liliakanukova <115219418+liliakanukova@users.noreply.github.com>
2022-12-23 15:38:21 +0300

    Add files via upload

---
+++ Новая папка/test.py
@@ -0,0 +1,9 @@
+from project import *
+  
+def encrypt_vygner():
+    assert encrypt_vygner('забег','пришел' == 'шрйыжф')
+def decrypt_vigner():
+    assert encrypt_vygner('забег','шрйыжф' == 'пришел')
+
+def encrypt_cezar(message,key):
+    assert encrypt_vygner('место','прише' == 'шрйыжф')
